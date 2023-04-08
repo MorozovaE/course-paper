234878bb047de34df6ea758bd0bd024a834626aa
Liza Morozova <lizavetamorozova12@gmail.com>
2023-04-08 23:25:29 +0300

    split TaskDetails component

--- frontend/src/components/TaskDetails/TaskDetails.jsx
+++ frontend/src/components/TaskDetails/TaskDetails.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
-import { ReactComponent as PriorityFlagIcon } from "../../assets/icons/priorityFlag.svg";
-import { ReactComponent as BinIcon } from "../../assets/icons/bin.svg";
-import { ReactComponent as CalendarIcon } from "../../assets/icons/calendar.svg";
-import { ReactComponent as CategoryIcon } from "../../assets/icons/category.svg";
 
-import { priorityClasses } from "../../constans";
 import styles from "./taskDetails.module.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { convertDateToStr } from "../../utils/convetDateToStr";
-import { Checkbox } from "../Checkbox/Checkbox";
+
 import {
   getTask,
   tasksSelector,
@@ -17,6 +11,7 @@ import {
   taskSelector,
   setSelectedTask,
 } from "../../store/features/tasksSlice";
+import {TaskDetailsHeader} from "../TaskDetailsHeader/TaskDetailsHeader";
 
 export const TaskDetails = () => {
   const dispatch = useDispatch();
@@ -37,37 +32,7 @@ export const TaskDetails = () => {
 
       {selectedTaskId && (
         <div>
-          <header className={styles.headerContainer}>
-            <div>
-              <Checkbox task={task} />
-              <div
-                className={
-                  task.dateTime == null ? styles.defaultDate : styles.date
-                }
-              >
-                <CalendarIcon />
-                <span>{convertDateToStr(task.dateTime)}</span>
-              </div>
-
-              {task.Priority ? (
-                <div className={styles[priorityClasses[task.Priority.id]]}>
-                  <PriorityFlagIcon />
-                  <span>{task.Priority.name}</span>
-                </div>
-              ) : (
-                <div className={styles.defaultPriority}>
-                  <PriorityFlagIcon className={styles.priority} />
-                </div>
-              )}
-
-              <div className={styles.category}>
-                <CategoryIcon />
-                {task.List && <span>{task.List.name}</span>}
-              </div>
-            </div>
-            <BinIcon className={styles.bin} />
-          </header>
-
+          <TaskDetailsHeader />
           <main>
             <input type="text" defaultValue={task.name} />
             <textarea

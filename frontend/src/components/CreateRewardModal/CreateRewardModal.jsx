5c2b714c3e6d4e99a1a3c5ebe658ca00af5523ba
Liza Morozova <lizavetamorozova12@gmail.com>
2023-04-30 21:13:03 +0300

    added new features: added opportunity drag and drop, added photo, and new design

--- frontend/src/components/CreateRewardModal/CreateRewardModal.jsx
+++ frontend/src/components/CreateRewardModal/CreateRewardModal.jsx
@@ -3,6 +3,45 @@ import React from "react";
 import styles from "./createRewardModal.module.scss";
 
 export const CreateRewardModal = ({ active, setActive }) => {
+  const [_, setDrag] = React.useState(false);
+  const [file, setFile] = React.useState(null);
+  const [preview, setPreview] = React.useState();
+
+  React.useEffect(() => {
+    if (!file) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file]);
+
+  const dragStartHandler = (e) => {
+    e.preventDefault();
+    setDrag(true);
+  };
+  const dragLeaveHandler = (e) => {
+    e.preventDefault();
+    setDrag(false);
+  };
+
+  const onDropHandler = (e) => {
+    e.preventDefault();
+    let uploadedFile = e.dataTransfer.files[0];
+
+    setFile(uploadedFile);
+    setDrag(false);
+  };
+
+  const cancelHandler = () => {
+    //value from input and textarea
+    setActive(false);
+    setFile(null);
+  };
+
   return (
     <div
       className={active ? `${styles.modal} ${styles.active}` : styles.modal}
@@ -30,16 +69,42 @@ export const CreateRewardModal = ({ active, setActive }) => {
 
           <div className={styles.modalRewardDescContainer}>
             <h4>Описание</h4>
-            <textarea name="" id="" cols="30" rows="5" placeholder="напиши свои мысли о награде..."></textarea>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="5"
+              placeholder="напиши свои мысли о награде..."
+            ></textarea>
           </div>
 
           <div className={styles.modalRewardImgContainer}>
             <h4>Картинка</h4>
-            <div>пертащите</div>
+            <div
+              className={`${styles.dropArea} ${file ? styles.uploaded : ""}`}
+              onDragStart={(e) => dragStartHandler(e)}
+              onDragEnd={(e) => dragLeaveHandler(e)}
+              onDragOver={(e) => dragStartHandler(e)}
+              onDrop={(e) => onDropHandler(e)}
+            >
+              {file ? (
+                <div
+                  className={styles.image}
+                  style={{ backgroundImage: `url(${preview})` }}
+                ></div>
+              ) : (
+                <div>Перетащите или загрузите картинку</div>
+              )}
+            </div>
           </div>
 
           <div className={styles.buttonsContainer}>
-            <button className={styles.cancelBtn}>Отмена</button>
+            <button
+              onClick={() => cancelHandler()}
+              className={styles.cancelBtn}
+            >
+              Отмена
+            </button>
             <button className={styles.createBtn}>Создать</button>
           </div>
         </div>

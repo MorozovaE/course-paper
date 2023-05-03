a38bdf8b51e28a3211968d3a09090ef378bf4bea
Liza Morozova <lizavetamorozova12@gmail.com>
2023-05-03 21:39:14 +0300

    added to store reducer reward

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasks from "./features/tasksSlice";
+import rewards from "./features/rewardsSlice"
 
 export const store = configureStore({
   reducer: {
     tasks,
+    rewards,
   },
 });

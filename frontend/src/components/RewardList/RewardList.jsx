ddad9ca098e0d0be21cf9c8b752a09a0c30ff822
Liza Morozova <lizavetamorozova12@gmail.com>
2023-05-03 21:33:32 +0300

    Download all awards and display them. Also added to rewardList scroll bar

--- frontend/src/components/RewardList/RewardList.jsx
+++ frontend/src/components/RewardList/RewardList.jsx
@@ -4,15 +4,23 @@ import styles from "./rewardList.module.scss";
 import { ReactComponent as PlusIcon } from "../../assets/icons/plus.svg";
 import { ReactComponent as SearchIcon } from "../../assets/icons/search.svg";
 import { ReactComponent as SortIcon } from "../../assets/icons/sort.svg";
-import { ReactComponent as DownloadIcon } from "../../assets/icons/downloadArrow.svg";
 
 import { Reward } from "../Reward/Reward";
 import { CreateRewardModal } from "../CreateRewardModal/CreateRewardModal";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllRewards,
+  rewardsSelector,
+} from "../../store/features/rewardsSlice";
 
 export const RewardList = () => {
+  const dispatch = useDispatch();
   const [modalActive, setModalActive] = React.useState(false);
-  // const inputRef = React.useRef();
-  // const [inputRewardName, setInputRewardName] = React.useState("");
+  const items = useSelector(rewardsSelector);
+
+  React.useEffect(() => {
+    dispatch(getAllRewards());
+  }, []);
 
   return (
     <div className={styles.root}>
@@ -28,13 +36,17 @@ export const RewardList = () => {
           </div>
           <CreateRewardModal active={modalActive} setActive={setModalActive} />
         </div>
+
         <div>
           <SearchIcon />
           <SortIcon />
         </div>
       </header>
-      <div>
-        <Reward />
+
+      <div className={styles.rewardsContainer}>
+        {items.map((reward) => (
+          <Reward key={reward.id} reward={reward} />
+        ))}
       </div>
     </div>
   );
